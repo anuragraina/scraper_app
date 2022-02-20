5648b0347d3f73fd3934a73665b0cfa83da7ff2b
Anurag Raina <rainakalhan90@gmail.com>
2022-02-20 18:46:04 +0530

    Add extractEvent function for 2nd website

--- app/db/add-events.js
+++ app/db/add-events.js
@@ -9,17 +9,21 @@ const addEvents = async () => {
 	try {
 		await mongoose.connect('mongodb://localhost:27017/test');
 
-		const eventsList = await scrapeWebsite(scrapeConfig[0]);
+		const eventsList = await scrapeWebsite(scrapeConfig[1]);
 
 		if (eventsList.length > 0) {
-			Event.insertMany(sanitizeEventsData(eventsList), function (err, docs) {
-				if (err) {
-					console.log('Encountered error while inserting events to db!');
-					console.error(err);
-				} else {
-					console.log(`${docs.length} events inserted to db successfully.`);
+			Event.insertMany(
+				sanitizeEventsData(eventsList),
+				{ ordered: false },
+				function (err, docs) {
+					if (err) {
+						console.log('Encountered error while inserting events to db!');
+						console.error(err);
+					} else {
+						console.log(`${docs.length} events inserted to db successfully.`);
+					}
 				}
-			});
+			);
 		} else {
 			console.log('No data received!!!');
 		}
--- app/utils/scrapeConfig.js
+++ app/utils/scrapeConfig.js
@@ -19,5 +19,18 @@ export const scrapeConfig = [
 	{
 		website: 'https://www.techmeme.com/events',
 		eventsContainerSelectors: 'div.rhov > a',
+		extractEvent: (eventNode, website) => {
+			const dates = eventNode.children.item(0).textContent.split('-');
+			const date = dates[0].concat(' 2022');
+			const eventName = eventNode.children.item(1).textContent;
+			const location = eventNode.children.item(2).textContent;
+
+			return {
+				website,
+				eventName,
+				date,
+				location,
+			};
+		},
 	},
 ];
