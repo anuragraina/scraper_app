38b9d4ce1bf1f6aa42b842343ee1779454765cc2
Anurag Raina <rainakalhan90@gmail.com>
2022-02-21 00:31:55 +0530

    Separate Table Headers from main Table

--- client/src/components/EventsTable.js
+++ client/src/components/EventsTable.js
@@ -1,21 +1,14 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
+
+import { EnhancedTableHead, EnhancedTableToolbar } from './TableComps';
 
 function createData(name, calories, fat, carbs, protein) {
 	return {
@@ -59,94 +52,9 @@ function getComparator(order, orderBy) {
 		: (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-const headCells = [
-	{
-		id: 'eventName',
-		label: 'Event Name',
-	},
-	{
-		id: 'date',
-		label: 'Event Date',
-	},
-	{
-		id: 'location',
-		label: 'Location',
-	},
-	{
-		id: 'websiteName',
-		label: 'Website Name',
-	},
-];
-
-function EnhancedTableHead(props) {
-	const { order, orderBy, onRequestSort } = props;
-	const createSortHandler = property => event => {
-		onRequestSort(event, property);
-	};
-
-	return (
-		<TableHead>
-			<TableRow>
-				{headCells.map(headCell => (
-					<TableCell
-						key={headCell.id}
-						align='left'
-						padding='normal'
-						sortDirection={orderBy === headCell.id ? order : false}
-					>
-						<TableSortLabel
-							active={orderBy === headCell.id}
-							direction={orderBy === headCell.id ? order : 'asc'}
-							onClick={createSortHandler(headCell.id)}
-						>
-							{headCell.label}
-							{orderBy === headCell.id ? (
-								<Box component='span' sx={visuallyHidden}>
-									{order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-								</Box>
-							) : null}
-						</TableSortLabel>
-					</TableCell>
-				))}
-			</TableRow>
-		</TableHead>
-	);
-}
-
-EnhancedTableHead.propTypes = {
-	onRequestSort: PropTypes.func.isRequired,
-	order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-	orderBy: PropTypes.string.isRequired,
-};
-
-const EnhancedTableToolbar = () => {
-	return (
-		<Toolbar
-			sx={{
-				pl: { sm: 2 },
-				pr: { xs: 1, sm: 1 },
-			}}
-		>
-			<Typography sx={{ flex: '1 1 100%' }} variant='h6' id='tableTitle' component='div'>
-				Tech Events
-			</Typography>
-
-			<Tooltip title='Filter list'>
-				<IconButton>
-					<FilterListIcon />
-				</IconButton>
-			</Tooltip>
-		</Toolbar>
-	);
-};
-
-EnhancedTableToolbar.propTypes = {
-	numSelected: PropTypes.number.isRequired,
-};
-
 export default function EnhancedTable() {
 	const [order, setOrder] = useState('asc');
-	const [orderBy, setOrderBy] = useState('calories');
+	const [orderBy, setOrderBy] = useState('date');
 	const [page, setPage] = useState(0);
 	const [rowsPerPage, setRowsPerPage] = useState(5);
 
---
+++ client/src/components/TableComps.js
@@ -0,0 +1,97 @@
+import PropTypes from 'prop-types';
+import TableHead from '@mui/material/TableHead';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import Box from '@mui/material/Box';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+const headCells = [
+	{
+		id: 'eventName',
+		label: 'Event Name',
+	},
+	{
+		id: 'date',
+		label: 'Event Date',
+	},
+	{
+		id: 'location',
+		label: 'Location',
+	},
+	{
+		id: 'websiteName',
+		label: 'Website Name',
+	},
+];
+
+export function EnhancedTableHead(props) {
+	const { order, orderBy, onRequestSort } = props;
+	const createSortHandler = property => event => {
+		onRequestSort(event, property);
+	};
+
+	return (
+		<TableHead>
+			<TableRow>
+				{headCells.map(headCell => (
+					<TableCell
+						key={headCell.id}
+						align='left'
+						padding='normal'
+						sortDirection={orderBy === headCell.id ? order : false}
+					>
+						<TableSortLabel
+							active={orderBy === headCell.id}
+							direction={orderBy === headCell.id ? order : 'asc'}
+							onClick={createSortHandler(headCell.id)}
+						>
+							{headCell.label}
+							{orderBy === headCell.id ? (
+								<Box component='span' sx={visuallyHidden}>
+									{order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+								</Box>
+							) : null}
+						</TableSortLabel>
+					</TableCell>
+				))}
+			</TableRow>
+		</TableHead>
+	);
+}
+
+EnhancedTableHead.propTypes = {
+	onRequestSort: PropTypes.func.isRequired,
+	order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+	orderBy: PropTypes.string.isRequired,
+};
+
+export const EnhancedTableToolbar = () => {
+	return (
+		<Toolbar
+			sx={{
+				pl: { sm: 2 },
+				pr: { xs: 1, sm: 1 },
+			}}
+		>
+			<Typography sx={{ flex: '1 1 100%' }} variant='h6' id='tableTitle' component='div'>
+				Tech Events
+			</Typography>
+
+			<Tooltip title='Filter list'>
+				<IconButton>
+					<FilterListIcon />
+				</IconButton>
+			</Tooltip>
+		</Toolbar>
+	);
+};
+
+EnhancedTableToolbar.propTypes = {
+	numSelected: PropTypes.number.isRequired,
+};
