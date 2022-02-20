d7bb4f463a2b6cc8bc7937757aaad4c93415d9aa
Anurag Raina <rainakalhan90@gmail.com>
2022-02-21 01:20:30 +0530

    Integrate UI with API

--- client/src/components/EventsTable.js
+++ client/src/components/EventsTable.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,32 +10,6 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 import { EnhancedTableHead, EnhancedTableToolbar } from './TableComps';
-
-function createData(name, calories, fat, carbs, protein) {
-	return {
-		name,
-		calories,
-		fat,
-		carbs,
-		protein,
-	};
-}
-
-const rows = [
-	createData('Cupcake', 305, 3.7, 67),
-	createData('Donut', 452, 25.0, 51),
-	createData('Eclair', 262, 16.0, 24),
-	createData('Frozen yoghurt', 159, 6.0, 24),
-	createData('Gingerbread', 356, 16.0, 49),
-	createData('Honeycomb', 408, 3.2, 87),
-	createData('Ice cream sandwich', 237, 9.0, 37),
-	createData('Jelly Bean', 375, 0.0, 94),
-	createData('KitKat', 518, 26.0, 65),
-	createData('Lollipop', 392, 0.2, 98),
-	createData('Marshmallow', 318, 0, 81),
-	createData('Nougat', 360, 19.0, 9),
-	createData('Oreo', 437, 18.0, 63),
-];
 
 function descendingComparator(a, b, orderBy) {
 	if (b[orderBy] < a[orderBy]) {
@@ -56,7 +31,23 @@ export default function EnhancedTable() {
 	const [order, setOrder] = useState('asc');
 	const [orderBy, setOrderBy] = useState('date');
 	const [page, setPage] = useState(0);
-	const [rowsPerPage, setRowsPerPage] = useState(5);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+	const [events, setEvents] = useState([]);
+
+	console.log(events);
+
+	useEffect(() => {
+		let mounted = true;
+
+		const fetchevents = async () => {
+			const response = await axios('/api/events');
+			mounted && setEvents(response.data);
+		};
+
+		fetchevents();
+
+		return () => (mounted = false);
+	}, []);
 
 	const handleRequestSort = (event, property) => {
 		const isAsc = orderBy === property && order === 'asc';
@@ -74,7 +65,7 @@ export default function EnhancedTable() {
 	};
 
 	// Avoid a layout jump when reaching the last page with empty rows.
-	const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+	const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - events.length) : 0;
 
 	return (
 		<Box sx={{ width: '100%' }}>
@@ -86,20 +77,19 @@ export default function EnhancedTable() {
 							order={order}
 							orderBy={orderBy}
 							onRequestSort={handleRequestSort}
-							rowCount={rows.length}
+							rowCount={events.length}
 						/>
 						<TableBody>
-							{rows
+							{events
 								.slice()
 								.sort(getComparator(order, orderBy))
 								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-								.map(row => (
-									<TableRow hover key={row.name}>
-										<TableCell>{row.name}</TableCell>
-										<TableCell>{row.calories}</TableCell>
-										<TableCell>{row.fat}</TableCell>
-										<TableCell>{row.carbs}</TableCell>
-										<TableCell>{row.protein}</TableCell>
+								.map(event => (
+									<TableRow hover key={event.eventName}>
+										<TableCell>{event.eventName}</TableCell>
+										<TableCell>{event.date}</TableCell>
+										<TableCell>{event.location}</TableCell>
+										<TableCell>{event.websiteName}</TableCell>
 									</TableRow>
 								))}
 							{emptyRows > 0 && (
@@ -117,7 +107,7 @@ export default function EnhancedTable() {
 				<TablePagination
 					rowsPerPageOptions={[5, 10, 25]}
 					component='div'
-					count={rows.length}
+					count={events.length}
 					rowsPerPage={rowsPerPage}
 					page={page}
 					onPageChange={handleChangePage}
