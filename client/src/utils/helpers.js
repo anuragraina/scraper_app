6eb3ad3dc64c780becfc1aec2d003f7fe81146d4
Anurag Raina <rainakalhan90@gmail.com>
2022-02-23 03:51:56 +0530

    Bug Fix: Remove date param when no date selected

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -16,16 +16,17 @@ export const getFiltersConfig = () => {
 			value: [null, null],
 			tempValue: [null, null],
 			getSearchString: dates => {
-				if (dates.length > 0) {
-					const newDates = dates.map(date => {
-						if (date !== null) {
-							return format(date, 'yyyy-MM-dd');
-						}
-						return null;
-					});
-					return '&date='.concat(newDates.join(','));
+				const newDates = dates.map(date => {
+					if (date !== null) {
+						return format(date, 'yyyy-MM-dd');
+					}
+					return null;
+				});
+
+				if (newDates[0] === null && newDates[1] === null) {
+					return '';
 				}
-				return '';
+				return '&date='.concat(newDates.join(','));
 			},
 		},
 	};
