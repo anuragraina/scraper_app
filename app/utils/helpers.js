a2633802f12fb1da8f6b9bcc8c0920bce1840b50
Anurag Raina <rainakalhan90@gmail.com>
2022-02-23 03:17:09 +0530

    Add date filter at server

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -11,9 +11,23 @@ export const sanitizeEventsData = eventsList => {
 };
 
 export const queryDb = async queryParams => {
-	const { locationType } = queryParams;
+	const { locationType, date } = queryParams;
 
 	const queryObject = {};
+
+	if (date) {
+		const dates = date.split(',');
+
+		if (dates[0].length > 0) {
+			const startDate = new Date(dates[0]);
+			queryObject.date = { ...queryObject.date, $gte: startDate };
+		}
+
+		if (dates[1].length > 0) {
+			const endDate = new Date(dates[1]);
+			queryObject.date = { ...queryObject.date, $lte: endDate };
+		}
+	}
 
 	if (locationType === 'Virtual') {
 		queryObject.location = '';
